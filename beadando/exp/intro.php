6a9b5389382bd7fc8614909d8c349f4d29b18472
rikerik <bako.erik.dominik@gmail.com>
2022-12-15 23:52:33 +0100

    alapok pipa

---
+++ beadando/exp/intro.php
@@ -0,0 +1,13 @@
+<?php
+<body>
+    <header class="banner">
+        <h1>Üdvözöllek az Eperfa tájház kapujában!</h1>
+    </header>
+    <div class="topnav">
+        <a class="active" href="index.php">Főoldal</a>
+        <a href="galeria.php">Galéria</a>
+        <a href="kapcsolat.php">Kapcsolat</a>
+        <a href="informaciok.php">Információk</a>
+    </div>
+</body>
+?>
