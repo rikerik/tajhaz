0a12cce2ec3b821e9959622daa019d3d6505cb97
rikerik <bako.erik.dominik@gmail.com>
2022-12-18 20:23:15 +0100

    galéria 80%

--- beadando/exp/intro.php
+++ beadando/exp/intro.php
@@ -6,6 +6,7 @@ echo '
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="exp/style.css">
+
     <title>Eperfa tájház</title>
 </head>
 
--- beadando/galeria.php
+++ beadando/galeria.php
@@ -2,9 +2,60 @@
 <html lang="en">
 
 <body>
-
     <?php include "exp/intro.php"; ?>
+    <div class="galleryText">
+        <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, minima.</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa quia, velit odit aliquid veniam architecto, dolore quibusdam accusantium soluta asperiores beatae? Deleniti assumenda magnam debitis!</p>
+    </div>
 
+
+    <ul class="gallery">
+        <li>
+            <a href="gallery/1.jpg" target="_blank" title="Megtekintés">
+                <img src="gallery/1.jpg" alt="tájház 1">
+            </a>
+        </li>
+        <li>
+            <a href="gallery/2.jpg" target="_blank" title="Megtekintés">
+                <img src="gallery/2.jpg" alt="tájház 2">
+            </a>
+        </li>
+        <li>
+            <a href="gallery/3.jpg" target="_blank" title="Megtekintés">
+                <img src="gallery/3.jpg" alt="tájház 3">
+            </a>
+        </li>
+        <li>
+            <a href="gallery/4.jpg" target="_blank" title="Megtekintés">
+                <img src="gallery/4.jpg" alt="tájház 4">
+            </a>
+        </li>
+        <li>
+            <a href="gallery/5.jpg" target="_blank" title="Megtekintés">
+                <img src="gallery/5.jpg" alt="tájház 5">
+            </a>
+        </li>
+        <li>
+            <a href="gallery/6.jpg" target="_blank" title="Megtekintés">
+                <img src="gallery/6.jpg" alt="tájház 6">
+            </a>
+        </li>
+        <li>
+            <a href="gallery/7.jpg" target="_blank" title="Megtekintés">
+                <img src="gallery/7.jpg" alt="tájház 7">
+            </a>
+        </li>
+        <li>
+            <a href="gallery/8.jpg" target="_blank" title="Megtekintés">
+                <img src="gallery/8.jpg" alt="tájház 8">
+            </a>
+        </li>
+        <li>
+            <a href="gallery/9.jpg" target="_blank" title="Megtekintés">
+                <img src="gallery/9.jpg" alt="tájház 9">
+            </a>
+        </li>
+    </ul>
 </body>
 
 </html>
