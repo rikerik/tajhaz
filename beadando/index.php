28780fd2cc29fb7fcd680b9fbe7ce24eea3bb5c7
rikerik <bako.erik.dominik@gmail.com>
2022-12-21 15:22:25 +0100

    info
    kisebb javítások és animációk

--- beadando/index.php
+++ beadando/index.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php include "exp/intro.php"; ?>
+
 <body>
-    <h1>Lorem ipsum dolor sit amet.</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam nisi enim unde a inventore, tempore culpa nemo iste et commodi tenetur fugit laborum. Eaque, minus!</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, sint?</p>
+    <div class="mainText">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam nisi enim unde a inventore, tempore culpa nemo iste et commodi tenetur fugit laborum. Eaque, minus!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, sint?</p>
+    </div>
+
 </body>
 <?php include "exp/end.php"; ?>
 
--- beadando/informaciok.php
+++ beadando/informaciok.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php include "exp/intro.php"; ?>
-<body>
-<div">
-       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2780.884321017666!2d18.09943511556913!3d45.81357507910664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475d45c4dfe71c2b%3A0xa75b46059925eb0d!2zRXBlcmZhIFTDoWpow6F6!5e0!3m2!1shu!2shu!4v1671185073797!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
+<body>
+        <div class="csoport">
+                <img src="info/csoport1.jpg" alt="">
         </div>
+        <div class="infoSzoveg">
+                <h3>A tagokról:</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam quaerat earum aperiam voluptatem aliquam amet enim inventore qui molestias ipsam commodi nulla ea explicabo perspiciatis totam sunt sed perferendis repellat quidem, voluptatibus excepturi nostrum! Laboriosam aspernatur accusamus voluptatum officia architecto inventore rerum placeat vitae odio, expedita incidunt? Molestiae ipsam non facilis iste a in, esse quae beatae omnis eligendi. Quis, beatae velit voluptate vel neque aliquam a possimus temporibus repudiandae perferendis quasi sit odio omnis molestiae earum eaque ea ab, autem cumque quisquam dolorum dicta adipisci. Beatae laboriosam laborum vel accusamus facilis sit error nobis, maiores, nesciunt eligendi, quis debitis?</p>
+        </div>
+        <div">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2780.884321017666!2d18.09943511556913!3d45.81357507910664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475d45c4dfe71c2b%3A0xa75b46059925eb0d!2zRXBlcmZhIFTDoWpow6F6!5e0!3m2!1shu!2shu!4v1671185073797!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+                </div>
 </body>
 <?php include "exp/end.php"; ?>
+
 </html>
